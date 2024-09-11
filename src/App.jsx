f41bbfbb655c4f464f89f0e16c101cdc66faf98c
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-09-11 19:49:40 +0100

    Small styling change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import Homepage from "./Homepage/Homepage.jsx";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="bg-gray-300">
       <Homepage />
--- src/Homepage/WhatWeOffer.jsx
+++ src/Homepage/WhatWeOffer.jsx
@@ -1,6 +1,6 @@
 const WhatWeOffer = () => {
   return (
-    <div className="w-full h-screen bg-[url('./room.jpg')] bg-cover bg-center border-t-4 border-red-700 warehouse">
+    <div className="w-full h-[75vh] bg-[url('./room.jpg')] bg-cover bg-center border-t-4 border-red-700 warehouse">
       <div className="mt-8  text-white font-cinzel font-semibold pt-8">
         <h2 className="text-center text-5xl  underline underline-offset-8">
           What We <span className="font-bold text-red-500">Offer</span>
