8dc5cb41b6ed46a59594d2ac0f1f006c56e060f5
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-09-12 09:49:14 +0100

    Change What We Do to cards

--- src/Homepage/WhatWeDo.jsx
+++ src/Homepage/WhatWeDo.jsx
@@ -1,11 +1,11 @@
 const WhatWeDo = () => {
   return (
-    <div className=" text-black border-t-4 border-black">
-      <div className=" grid grid-cols-2">
-        <div className="w-full border-l-4 border-red-900">
+    <div className=" text-black border-t-4 pb-12 border-black">
+      <div className=" ">
+        {/* <div className="w-full border-l-4 border-red-900">
           {" "}
           <img className="" src="./warehouse.jpg" alt="" />
-        </div>
+        </div> */}
         <div className=" order-first font-livvic">
           <div className=" text-center rounded-xl p-12 ">
             <h2 className="text-4xl font-semibold  m-auto my-8 font-cinzel">
@@ -16,6 +16,75 @@ const WhatWeDo = () => {
               Lorem ipsum dolor sit amet. Sed vero modi eum quas unde ut
               provident distinctio ut provident mollitia quo quaerat temporibus.
             </h3>
+          </div>
+
+          <div className="mx-12 grid grid-cols-3 gap-12">
+            <div className="h-[80vh] border-2 border-dashed border-black bg-black rounded-3xl text-white">
+              <img
+                src="./room.jpg"
+                className="rounded-t-3xl border-b-4 border-red-700"
+                alt=""
+              />
+              <h3 className="text-center mt-4 text-2xl font-cinzel font-medium">
+                Wardrobe
+              </h3>
+
+              <h4 className="mt-6 px-8 font-livvic text-xl text-center">
+                Lorem ipsum dolor sit amet. Sed vero modi eum quas unde ut
+                provident distinctio ut provident mollitia quo quaerat
+                temporibus.
+              </h4>
+
+              <div className="flex">
+                <button className="m-auto mt-12 bg-red-700 font-cinzel font-medium text-lg">
+                  Learn More
+                </button>
+              </div>
+            </div>
+            <div className="h-[80vh] border-2 border-dashed border-black bg-black rounded-3xl text-white">
+              <img
+                src="./room.jpg"
+                className="rounded-t-3xl border-b-4 border-red-700"
+                alt=""
+              />
+              <h3 className="text-center mt-4 text-2xl font-cinzel font-medium">
+                Wardrobe
+              </h3>
+
+              <h4 className="mt-6 px-8 font-livvic text-xl text-center">
+                Lorem ipsum dolor sit amet. Sed vero modi eum quas unde ut
+                provident distinctio ut provident mollitia quo quaerat
+                temporibus.
+              </h4>
+
+              <div className="flex">
+                <button className="m-auto mt-12 bg-red-700 font-cinzel font-medium text-lg">
+                  Learn More
+                </button>
+              </div>
+            </div>
+            <div className="h-[80vh] border-2 border-dashed border-black bg-black rounded-3xl text-white">
+              <img
+                src="./room.jpg"
+                className="rounded-t-3xl border-b-4 border-red-700"
+                alt=""
+              />
+              <h3 className="text-center mt-4 text-2xl font-cinzel font-medium">
+                Wardrobe
+              </h3>
+
+              <h4 className="mt-6 px-8 font-livvic text-xl text-center">
+                Lorem ipsum dolor sit amet. Sed vero modi eum quas unde ut
+                provident distinctio ut provident mollitia quo quaerat
+                temporibus.
+              </h4>
+
+              <div className="flex">
+                <button className="m-auto mt-12 bg-red-700 font-cinzel font-medium text-lg">
+                  Learn More
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
